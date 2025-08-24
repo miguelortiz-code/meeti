b76c0518db1d6eeb095983587689917681f21801
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-08-24 15:29:02 -0500

    feat(db): configure database connection, create config/db.js and import in index.js.

---
+++ src/config/db.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+
+// Configuración de la base de datos
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
+    host: process.env.DB_SERVER,
+    dialect:'postgres',
+    port: process.env.DB_PORT,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    logging: false
+})
+
+// conexión a la base de datos
+const connection = async () =>{
+    try {
+        await db.authenticate();
+        console.log(`✅ Conexión exitosa a la base de datos ${process.env.DB_NAME}`);
+    } catch (error) {
+        console.error(`❌ Error al conectar a la base de datos : ${error}`);
+        process.exit(1); // Detiene la app si no conecta
+        
+    }
+}
+
+
+export default connection
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import expressEjsLayouts from 'express-ejs-layouts';
 import {home} from './routes/index.routes.js';
-
+import connection from './config/db.js'
 const app = express();
+
+// Connection database
+connection()
 
 // Habilitar EJS como template engine
 app.use(expressEjsLayouts);
