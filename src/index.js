7493eb044083817a3afb1020238f748df7b2e147
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-08-24 18:04:48 -0500

    chore(deps): install cookie-parser, express-session and connect-flash dependencies.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import expressEjsLayouts from 'express-ejs-layouts';
+import flash from 'connect-flash';
+import cookieParser from 'cookie-parser';
 import { home } from './routes/index.routes.js';
 import { connection, db } from './config/db.js';
 import {Users} from './models/index.model.js';
+import session from 'express-session';
 
 const app = express();
 
-// Habilitar lectura de datos de formularios
-app.use(express.urlencoded({extended: true}));
 
 const startApp = async () => {
   try {    
@@ -17,6 +18,23 @@ const startApp = async () => {
     // ahora registra los modelos y sincroniza
     await db.sync({ alter: true });
     console.log("✅ Tablas sincronizadas");
+    
+    // Habilitar lectura de datos de formularios
+    app.use(express.urlencoded({extended: true}));
+    
+    // Habilitar cookie Parser
+    app.use(cookieParser());
+    
+    // Crear la sesion
+    app.use(session({
+      secret: process.env.SECRET,
+      key: process.env.KEY,
+      resave: false,
+      saveUninitialized: false
+    }));
+
+    // Agregando FLASH
+    app.use(flash());
 
     // Habilitar EJS como template engine
     app.use(expressEjsLayouts);
