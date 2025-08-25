282133c59e6b92ecaf8723c5a5e4e5dbf0d0d317
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-08-25 18:38:18 -0500

    feat(auth): integrate nodemailer for account confirmation, add helpers/token-emails.js and send confirmation email from controller.

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,96 +1,134 @@
-import bcrypt from 'bcrypt';
-import { check, validationResult } from 'express-validator';
-import {Users} from '../models/index.model.js';
+import bcrypt from "bcrypt";
+import { check, validationResult } from "express-validator";
+import { Users } from "../models/index.model.js";
+import { generateId } from "../helpers/token.helper.js";
+import { emailRegister } from "../helpers/emails.helper.js";
 
 // Vista de la página principal
-const viewHome = (req, res) =>{
-    res.render('home',{
-        namePage: 'Inicio'
-    });
-}
+const viewHome = (req, res) => {
+  res.render("home", {
+    namePage: "Inicio",
+  });
+};
 
 // Vista para registrar usuario
-const viewRegister = (req, res) =>{
-    res.render('auth/register',{
-        namePage: 'Crear Cuenta',
-        user: {}
-    })
-}
+const viewRegister = (req, res) => {
+  res.render("auth/register", {
+    namePage: "Crear Cuenta",
+    user: {},
+  });
+};
 
 // Función para registrar usuario
 const register = async (req, res) => {
-    const { name, email, password, confirm_password } = req.body;
+  const { name, email, password, confirm_password } = req.body;
 
+  try {
     // Validaciones
-    await check('name').notEmpty().withMessage('El nombre es obligatorio').trim().escape().toLowerCase().run(req);
-    await check('email').notEmpty().withMessage('El correo es obligatorio').isEmail().withMessage('El formato del correo es inválido').normalizeEmail().run(req);
-    await check('password').notEmpty().withMessage('La contraseña es obligatoria').isLength({ min: 8 }).withMessage('La contraseña debe tener mínimo 8 caracteres').trim().run(req);
-    await check('confirm_password').notEmpty().withMessage('La confirmación de la contraseña es obligatoria')
-        .custom((val, { req }) => {
-            if (val !== req.body.password) {
-                throw new Error('Las contraseñas no coinciden');
-            }
-            return true;
-        }).trim().run(req);
+    await check("name")
+      .notEmpty()
+      .withMessage("El nombre es obligatorio")
+      .trim()
+      .escape()
+      .toLowerCase()
+      .run(req);
+    await check("email")
+      .notEmpty()
+      .withMessage("El correo es obligatorio")
+      .isEmail()
+      .withMessage("El formato del correo es inválido")
+      .normalizeEmail()
+      .run(req);
+    await check("password")
+      .notEmpty()
+      .withMessage("La contraseña es obligatoria")
+      .isLength({ min: 8 })
+      .withMessage("La contraseña debe tener mínimo 8 caracteres")
+      .trim()
+      .run(req);
+    await check("confirm_password")
+      .notEmpty()
+      .withMessage("La confirmación de la contraseña es obligatoria")
+      .custom((val, { req }) => {
+        if (val !== req.body.password) {
+          throw new Error("Las contraseñas no coinciden");
+        }
+        return true;
+      })
+      .trim()
+      .run(req);
 
     let result = validationResult(req);
 
     // Validar si hay errores
     if (!result.isEmpty()) {
-        req.flash('error', result.array().map(err => err.msg));
-        return res.render('auth/register', {
-            namePage: 'Crear Cuenta',
-            message: req.flash(),
-            user: { name, email }
-        });
+      req.flash(
+        "error",
+        result.array().map((err) => err.msg)
+      );
+      return res.render("auth/register", {
+        namePage: "Crear Cuenta",
+        message: req.flash(),
+        user: { name, email },
+      });
     }
 
-    try {
-        // Verificar usuario existente
-        const existUser = await Users.findOne({ where: { email } });
-        if (existUser) {
-            req.flash('error', 'El correo ya está registrado');
-            return res.render('auth/register', {
-                namePage: 'Crear Cuenta',
-                message: req.flash(),
-                user: { name, email }
-            });
-        }
-
-        // Hashear contraseña
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Guardar usuario
-        await Users.create({ name, email, password: hashedPassword });
-
-        // Mostrar mensaje y redireccionar
-        req.flash('exito', 'Hemos enviado un correo electrónico para confirmar tu cuenta');
-        return res.redirect('/login');
-
-    } catch (error) {
-        console.error(error);
-        req.flash('error', 'Hubo un problema al registrar el usuario');
-        return res.render('auth/register', {
-            namePage: 'Crear Cuenta',
-            message: req.flash(),
-            user: { name, email }
-        });
+    // Verificar usuario existente
+    const existUser = await Users.findOne({ where: { email } });
+    if (existUser) {
+      req.flash("error", "El correo ya está registrado");
+      return res.render("auth/register", {
+        namePage: "Crear Cuenta",
+        message: req.flash(),
+        user: { name, email },
+      });
     }
+
+    // Hashear contraseña
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Guardar usuario
+    const user = await Users.create({
+      name,
+      email,
+      password: hashedPassword,
+      token: generateId(),
+    });
+
+    // Enviar mensaje de confirmación
+    emailRegister({
+      name: user.name,
+      email: user.email,
+      token: user.token,
+    });
+
+    // Mostrar mensaje de confirmación
+    res.render("auth/confirm", {
+      namePage: "Cuenta Creada Correctamente",
+      message: {
+        exito: [
+          "Te hemos enviado un correo con un enlace de confirmación. Por favor, revisa tu bandeja de entrada y sigue las instrucciones para activar tu cuenta.",
+        ],
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    req.flash("error", "Hubo un problema al registrar el usuario");
+    return res.render("auth/register", {
+      namePage: "Crear Cuenta",
+      message: req.flash(),
+      user: { name, email },
+    });
+  }
 };
 
 // vista para login
-const viewLogin = (req, res) =>{
-    res.render('auth/login',{
-        namePage: 'Iniciar Sesión',
-        user: {}
-    });
+const viewLogin = (req, res) => {
+  res.render("auth/login", {
+    namePage: "Iniciar Sesión",
+    user: {},
+  });
 };
 
-
-export {
-    viewHome,
-    viewRegister,
-    register,
-    viewLogin
-}
+export { viewHome, viewRegister, register, viewLogin };
---
+++ src/helpers/emails.helper.js
@@ -0,0 +1,50 @@
+import nodemailer from 'nodemailer';
+
+// Función para confirmar cuenta
+const emailRegister = async(data) =>{
+    const transport =  nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    // Extraer datos del controller
+    const {name, email, token} = data;
+   
+    // Envio del correo
+    try {
+        await transport.sendMail({
+            from: '"Meeti" <no-reply@support.meeti.com',
+            to: email,
+            subject: 'Confirma tu cuenta en Meeti',
+            html: `
+                <div style="max-width: 600px; margin: 20px auto; padding: 30px; border: 1px solid #e1e1e1; border-radius: 10px; font-family: Arial, sans-serif; background-color: #f9f9f9; color: #333;">
+                    <h2 style="text-align: center; color: #2c3e50;">Meeti</h2>
+                    <p style="font-size: 16px;">Hola <strong>${name}</strong>,</p>
+                    <p style="font-size: 15px;">
+                        Gracias por registrarte en <strong>Meeti</strong>. Para completar tu registro, por favor confirma tu cuenta haciendo clic en el siguiente botón:
+                    </p>
+
+                    <div style="text-align: center; margin: 30px 0;">
+                        <a href="${process.env.backend_url}:${
+                        process.env.backend_port ?? 3000
+                        }/confirm-account/${token}" style="background-color: #3498db; color: #fff; padding: 12px 24px; text-decoration: none; border-radius: 5px; font-weight: bold;">Confirmar Cuenta</a>
+                    </div>
+
+                    <p style="font-size: 14px;">Si tú no creaste esta cuenta, puedes ignorar este mensaje.</p>
+
+                    <p style="font-size: 14px;">Atentamente,<br>El equipo de Meeti</p>
+                </div>
+            `,
+        });
+    } catch (error) {
+        console.error(`Error al enviar el correo: ${error}`);
+    }
+}
+
+export {
+    emailRegister
+}
