2c6658a34aae206d09f7c3a224b67c3144c42a84
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-08-29 21:01:29 -0500

    feat(groups): load categories from model in controller and render dynamically in view.

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,3 +1,5 @@
+import {Categories} from '../models/index.model.js';
+
 // Vista del panel principal
 const viewDashboard = (req, res) =>{
     res.render('admin/dashboard',{
@@ -6,9 +8,12 @@ const viewDashboard = (req, res) =>{
 }
 
 // Vista de formularios
-const formNewGruop = (req, res) =>{
+const formNewGruop = async (req, res, next) =>{
+    const categories = await Categories.findAll();
+
     res.render('admin/groups/new-group',{
-        namePage: 'Crea un nuevo grupo'
+        namePage: 'Crea un nuevo grupo',
+        categories
     })
 };
 
