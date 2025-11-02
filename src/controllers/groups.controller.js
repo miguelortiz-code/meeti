b8d9661ab8d2b2d390067d2394625b050fc05562
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-11-02 18:02:39 -0500

    feat: load categories dynamically from database in groups controller and display them.

--- src/controllers/groups.controller.js
+++ src/controllers/groups.controller.js
@@ -1,5 +1,10 @@
-export const viewNewGroup = (req, res) =>{
+import {Categories} from '../models/index.model.js';
+
+export const viewNewGroup = async (req, res) =>{
+   // Buscar todas las categorias
+   const categories  = await Categories.findAll();
    res.render('groups/new-group',{
-    namePage: 'Crear nuevo grupo'
+    namePage: 'Crear nuevo grupo',
+    categories
    }) 
 }
