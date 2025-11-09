e7271ba0ca9817903da9fdd222c7b5f7185ff192
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-11-08 19:42:36 -0500

    feat: display groups dynamically in dashboard and add new endpoints to complete CRUD operations.

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,5 +1,11 @@
-export const viewDashboard = (req, res) =>{
+import Groups from '../models/groups.models.js'
+
+export const viewDashboard = async (req, res) =>{
+    // Obtener todos los grupos
+    const groups = await Groups.findAll({where: {id_user: req.user.id}});
+    // Renderizar vista del dashboard
     res.render('admin/home',{
-        namePage: 'Panel Administrativo'
+        namePage: 'Panel Administrativo',
+        groups
     })
 }
