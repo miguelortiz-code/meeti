aabea4f3d59213d6e9b2b7e82b350a50186533ca
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-11-23 18:48:03 -0500

    feat: load creator-specific groups dynamically in meetis module.

--- src/controllers/meeties.controller.js
+++ src/controllers/meeties.controller.js
@@ -1,6 +1,11 @@
+import {Groups} from '../models/index.model.js';
+
 export const  viewNewMeeti = async (req, res) =>{
+   const groups = await Groups.findAll({where: {id_user: req.user.id}});
+
    res.render('meeties/new-meeti',{
     namePage: 'Crear tu Meeti',
+    groups,
     data: {}
    }) 
 }
