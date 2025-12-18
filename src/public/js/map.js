f14157b0eeb3e01b55376efe9c3cf555934ea653
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-12-17 21:31:22 -0500

    feat: add scripts to display map for meetis.

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -1,1 +1,11 @@
-alert("Empezando con leatLeft");
+(function () {
+  const lat = document.getElementById("lat").value || 4.651002988522;
+  const lng = document.getElementById("lng").value || -74.081282901615;
+  const map = L.map("mapa").setView([lat, lng], 12);
+  let marker;
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+})();
