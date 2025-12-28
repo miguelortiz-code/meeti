680da421444578d7cead7ca144791a56c148bf1c
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-12-27 23:38:06 -0500

    feat: add geocoding to retrieve location details from map pin.

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -3,6 +3,7 @@
   const lng = document.getElementById("lng").value || -74.081282901615;
   const map = L.map("mapa").setView([lat, lng], 12);
   let marker;
+  const geocodeService = L.esri.Geocoding.geocodeService(); // Utilizar provider y Geocoder
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
@@ -12,14 +13,24 @@
   // Pin o marker
   marker = new L.marker([lat, lng], {
     draggable: true,
-    autoPan: true
-  })
-  .addTo(map)
+    autoPan: true,
+  }).addTo(map);
 
   // Detectar el movimiento del marker
-  marker.on('moveend', function(e){
+  marker.on("moveend", function (e) {
     marker = e.target;
-    const  position =  marker.getLatLng();
+    const position = marker.getLatLng();
     map.panTo(new L.LatLng(position.lat, position.lng));
-  })
+
+    // Obtener la información de la calles al soltar el pin
+    geocodeService
+      .reverse()
+      .latlng(position, 13)
+      .run(function (error, result) {
+        if (error) {
+          console.log(error);
+        }
+        marker.bindPopup(result.address.LongLabel)
+      });
+  });
 })();
