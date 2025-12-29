0a15e0060ed3fd6d2f8bde9872acd2d7f45296d9
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-12-28 22:45:27 -0500

    feat: add form persistence with express-validator and integrate slug and shortid.

--- src/controllers/meeties.controller.js
+++ src/controllers/meeties.controller.js
@@ -1,4 +1,7 @@
 import striptags from "striptags";
+import slug from "slug";
+import shortid from "shortid";
+import {check, validationResult} from 'express-validator';
 import { Groups, Meeties } from "../models/index.model.js";
 
 // Mostrar la vista del meeti
@@ -16,10 +19,120 @@ export const viewNewMeeti = async (req, res) => {
 export const newMeetie = async (req, res, next) => {
   // Obtener Datos
   const description = striptags(req.body.description).trim();
+  
+  // Validaciones del formulario
+
+  await check('grupoId')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('El grupo no puede ir vacio')
+  .run(req)
+
+  await check('title')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('El titulo de la reunión no puede ir vacio')
+  .isLength({min_: 5})
+  .withMessage('El Meeti debe tener mínimo 5 caracteres')
+  .run(req);
+
+  await check('guest')
+  .trim()
+  .escape()
+  .run(req)
+
+  await check('date')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('Debes ingresar una fecha para la reunión')
+  .run(req)
+  
+  await check('hour')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('Debes ingresar una hora para la reunión')
+  .run(req)
+
+  await check("description")
+  .trim()
+  .notEmpty()
+  .withMessage("La descripción no puede ir vacía")
+  .isLength({ min: 15 })
+  .withMessage("La descripción debe tener mínimo 15 caracteres")
+  .run(req);
+
+  await check('country')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('El pais es obligatorio')
+  .run(req);
+
+  await check('city')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('La ciudad es obligaria')  
+  .run(req);
+
+  await check('zip_code')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('El código postal no puede ir vacio')
+  .run(req);
+
+  await check('address')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('La dirección no puede ir vacia')
+  .run(req);
+
+  await check('neighborhood')
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage('El nombre del barrio no puede ir vacio')
+  .run(req);
+  
+  await check('latitude')
+  .trim()
+  .escape()
+  .run(req);
+  
+  await check('longitude')
+  .trim()
+  .escape()
+  .run(req);
+
+  // Obtener los errores de validacion
+  const result = validationResult(req);
+
+  if(!result.isEmpty()){
+    // Agregar cada error como un mensaje flash independiente
+    result.array().forEach((err) => req.flash("error", err.msg));
+
+    // Volver a cargar los grupos
+    const groups = await Groups.findAll({ where: { id_user: req.user.id } });
+
+    // Mostrar nuevamente los errores
+    return res.render("meeties/new-meeti", {
+      namePage: "Crear tu Meeti",
+      groups,
+      messages: req.flash(),
+      data: (req.session.formData = req.body),
+    });
+  }
+
+  // Si no hay errores
   let quota = req.body.quota;
   const { id: id_user } = req.user;
   const { grupoId, title, guest, date, hour, country, city, zip_code, address, neighborhood, latitude, longitude} = req.body;
-
   // Cupo
   if (!quota || quota === "") {
     quota = 0;
