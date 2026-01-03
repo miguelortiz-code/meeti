ffbf95e3bec1017bdf74b6014d43a8a6b3eaaafb
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-01-02 23:03:50 -0500

    feat: add validations to edit profile form.

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,5 +1,6 @@
 import striptags from "striptags";
 import {Users} from '../models/index.model.js';
+import { check, validationResult } from "express-validator";
 
 // Vista para editar perfil
 export const viewProfile = async (req, res )=>{
@@ -8,7 +9,8 @@ export const viewProfile = async (req, res )=>{
     // Mostrar la vista para editar el perfil de usuario
     res.render('profile/edit-profile', {
         namePage: 'Editar Perfil',
-        user
+        user,
+        data: {}
     })
 }
 
@@ -16,6 +18,46 @@ export const viewProfile = async (req, res )=>{
 export const profile = async (req, res ) =>{
     const{code} = req.params; // Extaer codigo del usuario desde la url
     const user = await Users.findOne({where: {code}}); // Buscar usuario mediante el código
+
+    // Validaciones 
+    await check('name')
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage('El nombre de usuario no puede ir vacio')
+    .run(req)
+
+    await check("description")
+    .trim()
+    .notEmpty()
+    .withMessage("La descripción no puede ir vacía")
+    .isLength({ min: 15 })
+    .withMessage("La descripción debe tener mínimo 15 caracteres")
+    .run(req);
+
+    await check('email')
+    .trim()
+    .notEmpty()
+    .withMessage('El correo electronico no puede ir vacío')
+    .isEmail()
+    .withMessage('El correo electronico debe ser valido')
+    .run(req);
+
+    // Obtener los errores de validacion
+  const result = validationResult(req);
+
+  if(!result.isEmpty()){
+    // Agregar cada error como un mensaje flash independiente
+    result.array().forEach((err) => req.flash("error", err.msg));
+    const user = await Users.findOne({where: {code}}); // Buscar usuario por el code
+    // Mostrar nuevamente los errores
+    return res.render("profile/edit-profile", {
+      namePage: "Editar Perfil",
+      user,
+      messages: req.flash(),
+      data: (req.session.formData = req.body),
+    });
+  }
 
     // extaer información del formulario
     const description = striptags(req.body.description).trim();
