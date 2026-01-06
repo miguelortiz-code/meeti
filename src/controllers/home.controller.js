d52bcab31c520d76ec0c55dcd65f00ae1a12539f
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-01-05 22:37:50 -0500

    feat: fetch and display three nearest meetis on home with Groups and Users join.

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,15 +1,42 @@
-import {Categories} from '../models/index.model.js';
+import { Categories, Meeties, Groups, Users } from "../models/index.model.js";
+import moment from "moment";
+import { Op } from "sequelize";
 
-export const home = async (req, res)=>{
-    // Consulta multiple con Promise
-    const queries = [];
-    queries.push(Categories.findAll({}));
+export const home = async (req, res) => {
+  const queries = [];
 
-    // Extraer datos y pasar a la vista
-    const [categories] = await Promise.all(queries);
+  queries.push(Categories.findAll());
 
-    res.render('home/index',{
-        namePage: 'Inicio',
-        categories
+  queries.push(
+    Meeties.findAll({
+      attributes: ["slug", "title", "event_date", "hour"], // Columnas a mostrar
+      where: {
+        event_date: {
+          [Op.gte]: moment(new Date()).format('YYYY-MM-DD') // GTE para >= en las fechas
+        }
+      },
+      limit: 3,
+      order: [["event_date", "ASC"]],
+      include: [ // Join de groups and Users
+        {
+            model: Groups,
+            attributes: ['image']
+        },
+
+                {
+            model: Users,
+            attributes: ['name', 'image']
+        }
+      ]
     })
-}
+  );
+
+  const [categories, meeties] = await Promise.all(queries);
+
+  res.render("home/index", {
+    namePage: "Inicio",
+    categories,
+    meeties,
+    moment
+  });
+};
