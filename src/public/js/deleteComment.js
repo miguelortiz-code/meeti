589c500979f9d7fcd4290e95c192e4f6235a542d
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-01-28 22:19:42 -0500

    feat: add SweetAlert confirmation dialog for comment deletion view.

--- src/public/js/deleteComment.js
+++ src/public/js/deleteComment.js
@@ -1,22 +1,40 @@
-import axios from 'axios';
-import Swal from 'sweetalert2';
+import axios from "axios";
+import Swal from "sweetalert2";
 
-document.addEventListener('DOMContentLoaded' ,  () =>{
-    const DeleteForms = document.querySelectorAll('.eliminar-comentario');
+document.addEventListener("DOMContentLoaded", () => {
+  const DeleteForms = document.querySelectorAll(".eliminar-comentario");
 
-    // Revisar que exista los formularios
-    if(DeleteForms.length > 0){
-        DeleteForms.forEach(form => {
-            form.addEventListener('submit', deleteComment);
-        });
-    }
+  // Revisar que exista los formularios
+  if (DeleteForms.length > 0) {
+    DeleteForms.forEach((form) => {
+      form.addEventListener("submit", deleteComment);
+    });
+  }
 });
 
-function deleteComment (e){
-    e.preventDefault();
+function deleteComment(e) {
+  e.preventDefault();
 
-    axios.post(this.action)
-    .then(response =>{
+  Swal.fire({
+    title: "¿Deseas eliminar este comentario?",
+    text: "¡Un comentario eliminado, no se puede recuperar!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "¡Si. Eliminar!",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      axios.post(this.action).then((response) => {
         console.log(response);
-    });
+      });
+
+      Swal.fire({
+        title: "¡Eliminado!",
+        text: "El comentario ya no está disponible.",
+        icon: "success",
+      });
+    }
+  });
 }
